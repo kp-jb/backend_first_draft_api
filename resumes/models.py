2929db2b9669307bf444ce85777e6c7962ffec3d
Kiengchay Gomez <kphoutthavong@gmail.com>
2024-02-14 00:05:47 -0600

    define resume model

--- resumes/models.py
+++ resumes/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Resume(models.Model):
+    name = models.CharField(max_length=50)
+    # should this be called owner?
+    user_id = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, null=True, blank=True
+    )
+    content = models.TextField(blank=True)
+    created_date = models.TimeField()
+    modified_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        pass
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
