cf0081bee2e80d63ab436d7d62e1af6ab4698cea
jdabassett <jacobbassett@gmail.com>
2024-02-14 16:29:03 -0700

    untested login and registration

--- resumes/views.py
+++ resumes/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView
+)
+from .models import Resume
+from project.permissions import IsOwner
+from .serializers import ResumeSerializer
 
-# Create your views here.
+
+class ResumeListCreateView(ListCreateAPIView):
+    permission_classes = IsOwner
+    queryset = Resume.objects.all()
+    serializer_class = ResumeSerializer
+
+
+class ResumeRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    permission_classes = IsOwner
+    queryset = Resume.objects.all()
+    serializer_class = ResumeSerializer
