7615d7e42406c1e92c2a85fc5a57a1108750329f
jdabassett <jacobbassett@gmail.com>
2024-02-15 18:00:00 -0700

    getResumes now working

--- resumes/views.py
+++ resumes/views.py
@@ -8,12 +8,18 @@ from .serializers import ResumeSerializer
 
 
 class ResumeListCreateView(ListCreateAPIView):
-    permission_classes = IsOwner
+    permission_classes = [IsOwner]
     queryset = Resume.objects.all()
     serializer_class = ResumeSerializer
 
+    def get_queryset(self):
+        return Resume.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class ResumeRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
-    permission_classes = IsOwner
+    permission_classes = [IsOwner]
     queryset = Resume.objects.all()
     serializer_class = ResumeSerializer
